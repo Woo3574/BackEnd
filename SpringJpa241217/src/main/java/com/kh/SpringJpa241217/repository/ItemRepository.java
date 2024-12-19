554d9524a9ddedf96a72701efaf7b4ffa0b8cb12
Woo3574 <rlaxodn0429@gmail.com>
2024-12-19 11:51:33 +0900

    24.12.19

--- SpringJpa241217/src/main/java/com/kh/SpringJpa241217/repository/ItemRepository.java
+++ SpringJpa241217/src/main/java/com/kh/SpringJpa241217/repository/ItemRepository.java
@@ -4,7 +4,10 @@ import com.kh.SpringJpa241217.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     // 기본적인 CRUD는 이미 구현되어 있음
+    List<Item> findByItemNum(String itemNum);
 }
--- SpringJpa241217/src/test/java/com/kh/SpringJpa241217/repository/ItemRepositoryTest.java
+++ SpringJpa241217/src/test/java/com/kh/SpringJpa241217/repository/ItemRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,15 +25,27 @@ class ItemRepositoryTest {
     @Test
     @DisplayName("상품 저장 테스트") // 테스트 이름
     public void createItemTest() {
-        Item item = new Item(); // 빈 객체 만듬
-        item.setItemNum("테스트 상품");
-        item.setPrice(1000);
-        item.setItemDetail("테스트 상품 상세 설명");
-        item.setItemSellStatus(ItemSellStatus.SELL);
-        item.setStockNumber(100);
-        item.setRegTime(LocalDateTime.now());
-        item.setUpdateTime(LocalDateTime.now());
-        Item saveItem = itemRepository.save(item);
-        log.info("Item 저장 : {}", saveItem);
+        for (int i = 0; i <= 10; i++) {
+            Item item = new Item(); // 빈 객체 만듬
+            item.setItemNum("테스트 상품" + i);
+            item.setPrice(1000 * i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item saveItem = itemRepository.save(item);
+            log.info("Item 저장 : {}", saveItem);
+        }
+    }
+
+    @Test
+    @DisplayName("상품 조회 테스트")
+    public void findByItemNumTest() {
+        this.createItemTest(); // 10개의 상품을 생성'
+        List<Item> itemList = itemRepository.findByItemNum("테스트 상품5");
+        for(Item item : itemList) {
+            log.info("상품 조회 테스트 : {}", item);
+        }
     }
 }
