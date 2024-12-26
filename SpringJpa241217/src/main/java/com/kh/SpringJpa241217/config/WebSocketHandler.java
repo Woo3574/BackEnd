f051bda26202591dde6c61e776034619de23d3e1
Woo3574 <rlaxodn0429@gmail.com>
2024-12-26 12:29:08 +0900

    24.12.26 웹소켓

--- SpringJpa241217/src/main/java/com/kh/SpringJpa241217/config/WebSocketHandler.java
+++ SpringJpa241217/src/main/java/com/kh/SpringJpa241217/config/WebSocketHandler.java
@@ -1,6 +1,8 @@
 package com.kh.SpringJpa241217.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.SpringJpa241217.dto.ChatMessageDto;
+import com.kh.SpringJpa241217.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -17,6 +19,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component // 스프링 컨테이너에 빈 등록
 public class WebSocketHandler extends TextWebSocketHandler {
     private final ObjectMapper objectMapper;
+    private final ChatService chatService;
 
     // 세션과 채팅방을 매핑하는데 사용 (사용자가 어떤 채팅방에 속해 있는지 등록)
     private final Map<WebSocketSession, String>
@@ -27,6 +30,17 @@ public class WebSocketHandler extends TextWebSocketHandler {
             throws Exception {
         String payload = message.getPayload(); // 클라이언트가 전송한 메세지
         log.info("{}", payload);
+        // JSON 문자열을 ChatMessageDto 변환 작업
+        ChatMessageDto chatMessage = objectMapper.readValue(payload, ChatMessageDto.class);
+        String roomId = chatMessage.getRoomId();
+        if (chatMessage.getType() == ChatMessageDto.MessageType.ENTER) {
+            sessionRoomIdMap.put(session, chatMessage.getRoomId());
+            chatService.addSessionAndHandleEnter(roomId, session, chatMessage);
+        } else if (chatMessage.getType() == ChatMessageDto.MessageType.CLOSE) {
+            chatService.removeSessionAndHandleExit(roomId, session, chatMessage);
+        } else {
+            chatService.sendMessageToAll(roomId, chatMessage);
+        }
     }
 
     @Override // 웹소켓이 해제가 되었을 때
@@ -35,7 +49,9 @@ public class WebSocketHandler extends TextWebSocketHandler {
         log.info("연결 해제 이후 동작 : {}", session);
         String roomId = sessionRoomIdMap.remove(session);
         if (roomId != null) {
-
+            ChatMessageDto chatMessage = new ChatMessageDto();
+            chatMessage.setType(ChatMessageDto.MessageType.CLOSE);
+            chatService.removeSessionAndHandleExit(roomId, session, chatMessage);
         }
     }
 }
