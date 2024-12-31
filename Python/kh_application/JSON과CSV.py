52b678d6f22d168cedaf663ea3bafb7a07f28731
Woo3574 <rlaxodn0429@gmail.com>
2024-12-31 11:47:21 +0900

    2024.12.31

--- Python/kh_application/JSON과CSV.py
+++ Python/kh_application/JSON과CSV.py
@@ -47,3 +47,17 @@ print()
 # 범위 기반 for(로) cars 출력하기
 for car in cars:
     print(car, end=" ")
+
+print()
+
+for i in range(0, len(cars)):
+    print(f"{cars[i]}", end=" ")
+
+# CSV 파일 : Comma, Separator-Value, 콤마로 구분된 텍스트 형태의 파일
+import csv
+f = open("output.csv", "w", encoding="utf-8", newline="")
+wr = csv.writer(f) # f는 파일 객체
+wr.writerow([1, "안유진", "서울시 강남구 삼성동", "리더"])
+wr.writerow([2, "장원영", "서울시 강남구 역삼동", "센터"])
+wr.writerow([3, "이서", "서울시 강남구 신사동", "막내"])
+f.close()
---
+++ Python/kh_application/JSON과SpringBoot.py
@@ -0,0 +1,27 @@
+import requests # http 통신을 위한 모듈
+import json
+
+member = {
+    "email": "test@naver.com",
+    "pwd": "qwer1234",
+    "name": "안유진",
+    "imgPath": ""
+}
+
+# Server URL 및 Header 설정
+url = "http://localhost:8111/auth/signup"
+headers = {"Content-Type": "application/json"}
+
+# POST 요청 보내기
+response = requests.post(url, data=json.dumps(member), headers=headers)
+
+# 응답 처리
+if response.status_code == 200:
+    # print("회원 가입에 성공 했습니다.")
+    # Server(에서) 전달 받은 JSON(을) 객체로 변환 하고 내용을 출력
+    data = response.json()
+    print(f"이메일 : {data['email']}")
+    print(f"이름 : {data['name']}")
+    print(f"가입일 : {data['regDate']}")
+else:
+    print("회원 가입에 실패 했습니다.")
