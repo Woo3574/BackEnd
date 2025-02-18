708e5e904428bec185ea4050055d87eaccf07643
Woo3574 <rlaxodn0429@gmail.com>
2025-02-18 10:45:21 +0900

    2025.02.18.머신러닝

---
+++ Python/kh_빅데이터_250210/연속적인값예측.py
@@ -0,0 +1,59 @@
+# 회귀는 입력 데이타(독립변수)를 기반으로 출력변수(종속변수)를 예측하는 모델을 만드는 과정
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+import matplotlib.pyplot as plt
+
+perch_length = np.array([8.4, 13.7, 15.0, 16.2, 17.4, 18.0, 18.7, 19.0, 19.6, 20.0, 21.0,
+       21.0, 21.0, 21.3, 22.0, 22.0, 22.0, 22.0, 22.0, 22.5, 22.5, 22.7,
+       23.0, 23.5, 24.0, 24.0, 24.6, 25.0, 25.6, 26.5, 27.3, 27.5, 27.5,
+       27.5, 28.0, 28.7, 30.0, 32.8, 34.5, 35.0, 36.5, 36.0, 37.0, 37.0,
+       39.0, 39.0, 39.0, 40.0, 40.0, 40.0, 40.0, 42.0, 43.0, 43.0, 43.5,
+       44.0])
+perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0, 110.0,
+       115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0, 130.0,
+       150.0, 145.0, 150.0, 170.0, 225.0, 145.0, 188.0, 180.0, 197.0,
+       218.0, 300.0, 260.0, 265.0, 250.0, 250.0, 300.0, 320.0, 514.0,
+       556.0, 840.0, 685.0, 700.0, 700.0, 690.0, 900.0, 650.0, 820.0,
+       850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0, 1000.0,
+       1000.0])
+
+# 훈련 세트와 테스트 세트로 나눕니다.
+train_input, test_input, train_target, test_target = train_test_split(
+    perch_length, perch_weight, random_state=42)
+
+# 훈련 세트와 테스트 세트를 2차원 배열로 바꿉니다.
+train_input = train_input.reshape(-1, 1)
+test_input = test_input.reshape(-1, 1)
+
+knr = KNeighborsRegressor(n_neighbors=3)
+
+# k-최근접 이웃 회귀 모델을 훈련합니다.
+knr.fit(train_input, train_target)
+
+# 농어의 무게 예측하기
+print(knr.predict([[50]]))
+
+# 선형 회귀(Linear Regression)
+# - 입력 데이터와 출력데이터의 선형 관계를 학습
+# - 가장 적합한 직선을 찾아 새로운 데이터를 예측
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+
+# 선형 회귀 모델을 훈련합니다.
+lr.fit(train_input, train_target)
+# 50cm 농어에 대해 예측합니다.
+print(lr.predict([[50]]))
+
+# 훈련 세트의 산점도를 그립니다.
+plt.scatter(train_input, train_target)
+
+# 15에서 50까지 1차 방정식 그래프를 그립니다.
+plt.plot([15, 50], [15*lr.coef_+lr.intercept_, 50*lr.coef_+lr.intercept_])
+
+# 50cm 농어 데이터
+plt.scatter(50, 1241.8, marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
